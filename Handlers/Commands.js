79fa34d3144ab40897ba7e1e4d161d148baa9f81
Thomas <thomaspopoff@gmail.com>
2022-02-10 14:19:55 +0100

    first /ping command

---
+++ Handlers/Commands.js
@@ -0,0 +1,69 @@
+const { Perms } = require("../Validation/Permissions");
+const { Client } = require("discord.js")
+const { promisify } = require("util");
+const { glob } = require("glob");
+const PG = promisify(glob);
+const Ascii = require("ascii-table");
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+  const Table = new Ascii("Command Loaded");
+
+  const CommandsArray = [];
+
+  (await PG(`${process.cwd()}/Commands/*/*.js`)).map(async (file) => {
+    const command = require(file);
+
+    if(!command.name)
+    return Table.addRow(file.split("/")[7], "FAILED", "Missing a name.")
+
+    if(!command.description)
+    return Table.addRow(file.split("/")[7], "FAILED", "Missing a nadescription.me")
+
+    if(command.permission) {
+      if(Perms.includes(command.permission))
+      command.defaultPermission = false
+      else
+      return Table.addRow(command.name, "Failed", "Permission is invalid")
+    }
+
+    client.commands.set(command.name, command)
+    CommandsArray.push(command)
+
+    await Table.addRow(command.name, "SUCCESSFUL")
+
+  });
+
+  console.log(Table.toString())
+
+  // PERMISSIONS CHECK //
+
+  client.on('ready', async () => {
+    const MainGuild = await client.guilds.cache.get("933405606113591347")
+
+    MainGuild.commands.set(CommandsArray).then(async (command) => {
+      const Roles = (commandName) => {
+        const cmdPerms = CommandsArray.find((c) => c.name === commandName).permission;
+        if(!cmdPerms) return null;
+
+        return MainGuild.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+      }
+
+      const fullPermissions = command.reduce((accumulator, r) => {
+        const roles = Roles(r.name)
+        if(!roles) return accumulator;
+
+        const permissions = roles.reduce((a, r) => {
+          return [...a, {id : r.id, type: "ROLE", permission: true}]
+        }, [])
+
+
+        return [...accumulator, {id: r.id, permissions}]
+      },[])
+
+      await MainGuild.commands.permissions.set({ fullPermissions })
+    })
+  })
+}
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { Client, Intents } = require('discord.js')
+const { Client, Collection } = require('discord.js')
 const { Token } = require('./config.json')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const client = new Client({ intents: 32767 })
+
+client.commands = new Collection()
 
 require("./Handlers/Event")(client);
+require("./Handlers/Commands")(client);
 
 client.login(Token)
