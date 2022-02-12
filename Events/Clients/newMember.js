9d5136350b2b5ca164037e4bd3e367da590f6c7c
Thomas <thomaspopoff@gmail.com>
2022-02-12 21:48:25 +0100

    update message

--- Events/Clients/newMember.js
+++ Events/Clients/newMember.js
@@ -11,23 +11,23 @@ module.exports = {
     member.roles.add(NewMemberRole)
 
     const WelcomeMessage = new MessageEmbed()
-      .setColor('GREEN')
+      .setColor('RANDOM')
       .setDescription(`
         Bienvenue ${member} sur le Discord du club des Z'éléphants Volants !
 
         Ce discord est là pour nous permettre d'échanger des infos, de planifier des sorties et événements, de finaliser leurs organisations opérationnelles, et d'en faire le compte-rendu.
 
-        Afin de facilité l'organisation et la lisibilité, **il est souhaité que les pseudo affichés sur discord soient vos vrais prénoms et la première lettre de votre nom (au moins)**. Cela peut être paramètre pour le serveur des z'éléphants volants dans votre profil serveur.
+        Afin de facilité l'organisation et la lisibilité, **il est souhaité que les pseudo affichés sur discord soient les vrais prénoms et la première lettre du nom (au moins)**. Cela peut être paramètre pour le serveur des z'éléphants volants dans votre profil serveur.
 
-        Si vous n'êtes pas membre du club des Z'éléphants Volants, votre accès au serveur est limité à quelques salons. **Pour avoir un accès complet au serveur, vous devez faire valider votre licence FFVL à notre robot.** Il vous suffit d'écrire le message */licence* et de renseigner votre numéro de licence FFVL
+        Si tu n'êtes pas membre du club des Z'éléphants Volants, ton accès au serveur est limité à quelques salons. **Pour avoir un accès complet au serveur, tu dois faire valider ta licence FFVL à notre robot.** Il suffit de taper le message **/licence** dans un salon et de renseigner ton numéro de licence FFVL.
 
-        Vous êtes nouveau au club  ? Alors, n'hésitez pas à aller vous présenter dans le salon <#935798608396185641>
-        Vous voulez organiser une montée à Verel ? Rendez-vous dans <#935238034918621264>.
-        Vous voulez organiser une sortie un peu plus planifiée ? N'hésitez pas à créer un salon dans la catégorie Planification de sorties !
+        Tu es nouveau au club  ? Alors n'hésite pas à aller te présenter dans le salon <#935798608396185641>.
+        Tu veux organiser une montée à Vérel ? Rendez-vous dans <#935238034918621264>.
+        Tu veux organiser une sortie un peu plus planifiée ? N'hésite pas à créer un salon dans la catégorie Planification de sorties !
 
-        Si vous avez besoin d'aide sur l'outil Discord, le salon <#935247724088021002> est fait pour vous.
+        Si tu as besoin d'aide sur l'outil Discord, le salon <#935247724088021002> est fait pour toi.
 
-        Enfin, retrouvez tous les événements du club dans la catégorie Evénements calendriers et plein d'autres salons dans Vie du club
+        Enfin, retrouve tous les événements du club dans la catégorie Evénements calendriers et plein d'autres salons dans Vie du club.
 
         Bonnes discussions, et surtout bons vols !
       `)
