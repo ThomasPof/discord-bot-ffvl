079bc2cdaf07c6e7da6185bfd51402d7fbb69a4c
Thomas <thomaspopoff@gmail.com>
2022-02-14 18:55:49 +0100

    add verel command instead of webcam command

---
+++ Commands/Users/verel.js
@@ -0,0 +1,60 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js")
+
+const fetch = require('node-fetch');
+
+module.exports = {
+  name: "verel",
+  description: "Les infos utiles pour aller voler à Verel",
+  deferred: true,
+
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   */
+  execute(interaction) {
+    const Response = new MessageEmbed()
+    Response.setColor("GREEN")
+    Response.setTitle("Infos utiles pour Verel")
+
+    function degToCompass(deg) {
+      let val = parseInt(deg / 22.5);
+      console.log('val', (val % 16))
+      let arr = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+      return arr[(val % 16)]
+    }
+
+    fetch('http://api.pioupiou.fr/v1/live/800').then(response => response.json()).then((response) => {
+      console.log(response.data.meta.name)
+      Response.addField('\u200B',response.data.meta.name, false)
+      Response.addFields(
+        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
+        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) +  'km/h', inline: true },
+        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
+      )
+      interaction.editReply({ embeds:[Response], ephemeral: true });
+    })
+    fetch('http://api.pioupiou.fr/v1/live/641').then(response => response.json()).then((response) => {
+      console.log(response.data.meta.name)
+      Response.addField('\u200B',response.data.meta.name, false)
+      Response.addFields(
+        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
+        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) + ' km/h', inline: true },
+        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
+      )
+      interaction.editReply({ embeds:[Response], ephemeral: true });
+    })
+    fetch('http://api.pioupiou.fr/v1/live/456').then(response => response.json()).then((response) => {
+      console.log(response.data.meta.name)
+      Response.addField('\u200B',response.data.meta.name, false)
+      Response.addFields(
+        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
+        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) + ' km/h', inline: true },
+        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
+      )
+      interaction.editReply({ embeds:[Response], ephemeral: true });
+    })
+
+    Response.setImage('https://www.solarcam.fr/verel/last_big.jpg')
+    interaction.editReply({ embeds:[Response], ephemeral: true })
+  }
+}
--- Events/Interaction/interactionCreate.js
+++ Events/Interaction/interactionCreate.js
@@ -9,6 +9,7 @@ module.exports= {
    */
   async execute(interaction, client) {
     if(interaction.isCommand()) {
+
       const command = client.commands.get(interaction.commandName)
       if(!command) return interaction.reply({embeds: [
         new MessageEmbed()
@@ -16,6 +17,10 @@ module.exports= {
           .setDescription("An error occured while running this command.")
       ],ephemeral: true}) && client.commands.delete(interaction.commandName)
 
+      console.log(interaction)
+      if(command.deferred) {
+        await interaction.deferReply();
+      }
       command.execute(interaction, client)
     }
   }
