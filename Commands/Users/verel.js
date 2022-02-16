684c0d00dbce6cb5a2632f7773ce96b1b1323756
Thomas <thomaspopoff@gmail.com>
2022-02-16 19:42:25 +0100

    ajout cooldown on /verel cmd + refacto /verel cmd

--- Commands/Users/verel.js
+++ Commands/Users/verel.js
@@ -6,6 +6,8 @@ module.exports = {
   name: "verel",
   description: "Les infos utiles pour aller voler à Verel",
   deferred: true,
+  // cooldown in minutes
+  cooldown: 10,
 
   /**
    *
@@ -13,46 +15,33 @@ module.exports = {
    */
   execute(interaction) {
     const Response = new MessageEmbed()
+
+    const pioupious = [
+      'http://api.pioupiou.fr/v1/live/800',
+      'http://api.pioupiou.fr/v1/live/641',
+      'http://api.pioupiou.fr/v1/live/456'
+    ]
+
     Response.setColor("GREEN")
     Response.setTitle("Infos utiles pour Verel")
 
     function degToCompass(deg) {
       let val = parseInt(deg / 22.5);
-      console.log('val', (val % 16))
       let arr = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
       return arr[(val % 16)]
     }
 
-    fetch('http://api.pioupiou.fr/v1/live/800').then(response => response.json()).then((response) => {
-      console.log(response.data.meta.name)
-      Response.addField('\u200B',response.data.meta.name, false)
-      Response.addFields(
-        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
-        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) +  'km/h', inline: true },
-        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
-      )
-      interaction.editReply({ embeds:[Response], ephemeral: true });
-    })
-    fetch('http://api.pioupiou.fr/v1/live/641').then(response => response.json()).then((response) => {
-      console.log(response.data.meta.name)
-      Response.addField('\u200B',response.data.meta.name, false)
-      Response.addFields(
-        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
-        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) + ' km/h', inline: true },
-        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
-      )
-      interaction.editReply({ embeds:[Response], ephemeral: true });
-    })
-    fetch('http://api.pioupiou.fr/v1/live/456').then(response => response.json()).then((response) => {
-      console.log(response.data.meta.name)
-      Response.addField('\u200B',response.data.meta.name, false)
-      Response.addFields(
-        { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
-        { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) + ' km/h', inline: true },
-        { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
-      )
-      interaction.editReply({ embeds:[Response], ephemeral: true });
-    })
+    pioupious.forEach(function (item, index) {
+      fetch(item).then(response => response.json()).then((response) => {
+        Response.addField('\u200B',response.data.meta.name, false)
+        Response.addFields(
+          { name: 'Direction', value: degToCompass(response.data.measurements.wind_heading), inline: true  },
+          { name: 'Vent moyen', value: String(response.data.measurements.wind_speed_avg) +  'km/h', inline: true },
+          { name: 'Rafales', value: String(response.data.measurements.wind_speed_max) + ' km/h', inline: true },
+        )
+        interaction.editReply({ embeds:[Response], ephemeral: true });
+      })
+    });
 
     Response.setImage('https://www.solarcam.fr/verel/last_big.jpg')
     interaction.editReply({ embeds:[Response], ephemeral: true })
