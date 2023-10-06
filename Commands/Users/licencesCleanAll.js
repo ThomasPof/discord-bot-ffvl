e0ae387e18c6f7e546a68c4d20071db65b2da370
Thomas Popoff <thomaspopoff@gmail.com>
2023-10-06 08:25:10 +0200

    improve licence clean all function

--- Commands/Users/licencesCleanAll.js
+++ Commands/Users/licencesCleanAll.js
@@ -16,20 +16,25 @@ module.exports = {
 
     // On vérifie que le rôle pour l'année en cours existe
     const d = new Date();
-    const year = d.getFullYear();
+    let currentYear = d.getFullYear();
+    if(d.getMonth() >= 9) { // 9 = octobre
+      currentYear = currentYear + 1;
+    }
 
     const mainRole = guild.roles.cache.find(role => role.id == mainRoleId)
 
     Response.setColor("RED")
     Response.setDescription(TRANSLATION_LICENCE_CLEAN.successMessage())
+    let i = 0;
     guild.members.cache.forEach((member) => {
-      if(!member.roles.cache.some(role => role.name == 'Licencié '+year) &&
-         !member.roles.cache.some(role => role.name == 'Licencié '+(year - 1))
+      if(!member.roles.cache.some(role => role.name == 'Licencié '+currentYear) ||
+         !member.roles.cache.some(role => role.name == 'Licencié '+(currentYear - 1))
         ) {
-        // member.roles.remove(mainRole)
-        console.log('on retire le rôle');
+        i++
+        member.roles.remove(mainRole)
       }
     })
+    console.log('On retire le rôle à',i,'membres.');
 
     interaction.reply({embeds: [Response], ephemeral: true})
   }
